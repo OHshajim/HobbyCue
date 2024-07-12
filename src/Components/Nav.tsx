a275cca0984479733fba9e478cee626460779953
OHshajim <ajshajimmax@gmail.com>
2024-07-12 22:55:00 +0600

    nav add

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,9 +1,59 @@
+import Image from "next/image";
+
 const Nav = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="bg-[#1B1C1E]">
+      <div className="navbar bg-[#1B1C1E] container mx-auto">
+        <div className="flex-1">
+          <a className="btn btn-ghost ">
+            <Image src="/Logo.png" alt="logo" width={150} height={50} />
+          </a>
         </div>
-    );
+        <div className="flex-none gap-3">
+          <div className="w-10 rounded-full ">
+            <Image
+              alt="user photo"
+              src="/Search.png"
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className="w-10 rounded-full">
+            <Image
+              alt="user photo"
+              src="/Create.png"
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className="w-10 rounded-full">
+            <Image
+              alt="user photo"
+              src="/Message.png"
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className="w-10 rounded-full">
+            <Image
+              alt="user photo"
+              src="/Notification.png"
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className="w-10 rounded-full">
+            <Image
+              alt="user photo"
+              src="/Profile Pic (1).png"
+              width={50}
+              height={50}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Nav from "@/Components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <header>
-        
+    <main className="bg-base-100">
+      <header className=" ">
+        <Nav/>
       </header>
     </main>
   );
